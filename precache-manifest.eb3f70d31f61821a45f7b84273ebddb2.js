413f062d9af2ffe6f6cb9a53de23216779387b14
leehao92 <lee_haoo@163.com>
2019-02-09 11:15:22 +0800

    Updates

---
+++ precache-manifest.eb3f70d31f61821a45f7b84273ebddb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a1fa5a3438b67100dbe",
+    "url": "react-cnode-app/static/js/runtime~main.8a1fa5a3.js"
+  },
+  {
+    "revision": "6ec42e4ffefb2064f9a9",
+    "url": "react-cnode-app/static/js/main.6ec42e4f.chunk.js"
+  },
+  {
+    "revision": "400cb6af84792edad16c",
+    "url": "react-cnode-app/static/js/1.400cb6af.chunk.js"
+  },
+  {
+    "revision": "6ec42e4ffefb2064f9a9",
+    "url": "react-cnode-app/static/css/main.f7bae7ad.chunk.css"
+  },
+  {
+    "revision": "400cb6af84792edad16c",
+    "url": "react-cnode-app/static/css/1.2d6fe9a5.chunk.css"
+  },
+  {
+    "revision": "76db2d5100fb805f333d7ad98bf5d336",
+    "url": "react-cnode-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://LeeHao92.github.io/react-cnode-app/precache-manifest.6e7c74f58045aa6cbb298de37bcef7e3.js"
+  "react-cnode-app/precache-manifest.eb3f70d31f61821a45f7b84273ebddb2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://LeeHao92.github.io/react-cnode-app/index.html", {
+workbox.routing.registerNavigationRoute("react-cnode-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
